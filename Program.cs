ea987c905412e2810f0423a7e734a85e85c8c056
staslif <149203375+staslif@users.noreply.github.com>
2023-11-13 11:24:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите размер массива");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] num = new int[N];
+for (int i = 0; i < num.Length; i++)
+{
+    Console.WriteLine($" введите {i} элемент");
+    num[i] = Convert.ToInt32(Console.ReadLine());
+
+}
+int sum = 0;
+for (int i = 0; i < num.Length; i++)
+{
+    sum += i;
+}
+Console.WriteLine(sum);
